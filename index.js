534edead9282407073dc326bf8b102e82661afbf
Abhiramnanabolu <abhiramnanabolu0706@gmail.com>
2024-01-31 20:36:13 +0530

    start

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "chatapp.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+app.use(express.json());
+
+app.get("/", async (request, response) => {
+    const getBookQuery = `
+      SELECT
+        *
+      FROM
+        users`;
+    const book = await db.all(getBookQuery);
+    response.send(book);
+  });
