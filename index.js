061cf06e45273685f60c8caa21350eff901d8ece
Abhiramnanabolu <abhiramnanabolu0706@gmail.com>
2024-01-31 20:59:46 +0530

    1

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors")
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
-
+app.use(cors())
+const port=process.env.PORT || 3000
 const dbPath = path.join(__dirname, "chatapp.db");
 
 let db = null;
 
-const initializeDBAndServer = async () => {
+const initializeDBAndServer = async () => {ś
   try {
     db = await open({
       filename: dbPath,
